64737923962fdad5b40c85d99709b84ee339d795
brualvess <oliveirabrunna2210@gmail.com>
2022-04-16 20:24:53 -0300

    private message

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+let nome = "";
+function entrarNaSala() {
+  nome = prompt("Digite seu lindo nome");
+  promise = axios.post(
+    "https://mock-api.driven.com.br/api/v6/uol/participants",
+    { name: nome }
+  );
+  promise.then(verificarNome);
+}
+entrarNaSala();
+
+function verificarNome(aResposta) {
+  if (aResposta.status == 400) {
+    entrarNaSala();
+  }
+  console.log(aResposta);
+}
+
 function buscarMensagens() {
   const promessa = axios.get(
     "https://mock-api.driven.com.br/api/v6/uol/messages"
@@ -6,19 +24,23 @@ function buscarMensagens() {
 }
 setInterval(buscarMensagens, 3000);
 
+
 function processarMensagens(mensagens) {
   const mensagensDoArray = mensagens.data;
   const msgs = document.querySelector(".mensagens");
+  msgs.innerHTML = "";
 
   for (let i = 0; i < mensagensDoArray.length; i++) {
     let tipoDaMensagem = mensagensDoArray[i].type;
     switch (tipoDaMensagem) {
-      case "reservadas":
-        msgs.innerHTML += `<div class="mensagem reservadas">
-<div class="horario">${mensagensDoArray[i].time}</div>
-<div class="nome">${mensagensDoArray[i].from}</div>
-<div class="texto">${mensagensDoArray[i].text}</div>
-</div>`;
+      case "private_message":
+        if (nome == mensagensDoArray[i].to) {
+          msgs.innerHTML += `<div class="mensagem reservadas">
+  <div class="horario">${mensagensDoArray[i].time}</div>
+  <div class="nome">${mensagensDoArray[i].from}</div>
+  <div class="texto">${mensagensDoArray[i].text}</div>
+  </div>`;
+        }
         break;
       case "status":
         msgs.innerHTML += `<div class="mensagem status">
@@ -36,6 +58,7 @@ function processarMensagens(mensagens) {
         break;
     }
   }
-  console.log("hellooo");
-  console.log(mensagens);
+  let ultimaMensagem = document.querySelectorAll(".mensagem");
+  ultimaMensagem = ultimaMensagem[ultimaMensagem.length - 1];
+  ultimaMensagem.scrollIntoView();
 }
